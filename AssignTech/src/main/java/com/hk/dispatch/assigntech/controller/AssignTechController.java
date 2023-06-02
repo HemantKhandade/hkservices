f9433f59a4e21d7f8afe004bb80c741fb5bbbc89
Hemant K <hvlk369@gmail.com>
2023-06-02 10:39:35 +0530

    added response

---
+++ AssignTech/src/main/java/com/hk/dispatch/assigntech/controller/AssignTechController.java
@@ -0,0 +1,16 @@
+package com.hk.dispatch.assigntech.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AssignTechController {
+	
+	@GetMapping("/getassignedtech")
+	public ResponseEntity<String> getAssignedTech(){
+		return new ResponseEntity<String>("{'tech':'sd128b'}",HttpStatus.OK);
+	}
+
+}
